172cf4961b7df1839b98ec5b4e68bc061fd73909
Joshi Himanshu <joshihimanshu0717@gmail.com>
2026-04-02 13:31:10 +0530

    abc

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,27 @@
 import os
-
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from flask.cli import load_dotenv
 from pymongo import MongoClient
 
-
+# Load environment variables
 load_dotenv()
+
+# MongoDB connection
 mongo_uri = os.getenv("client")
 client = MongoClient(mongo_uri)
 
 db = client["todoDB"]
 collection = db["todoItems"]
 
+# Flask app
 app = Flask(__name__)
 
+# Home route
+@app.route('/')
+def form():
+    return render_template('todo.html')
+
+# Submit route
 @app.route('/submittodoitem', methods=['POST'])
 def submit_todo():
     item_name = request.form.get('itemName')
@@ -26,7 +34,10 @@ def submit_todo():
 
     collection.insert_one(data)
 
-    return jsonify({"message": "Todo item saved successfully"})
+    return jsonify({
+        "message": "Todo item saved successfully",
+        "data": data
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
